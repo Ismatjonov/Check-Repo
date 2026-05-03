242e275bd542857188c41aa1902925d339a8b38c
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-05-03 13:08:38 +0500

    Uploaded Memories)
    Lyceum Osimi 🥀

---
+++ Pluser/Pluser/Pluser.cpp
@@ -0,0 +1,16 @@
+﻿// Pluser.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    int a;
+    int b;
+    std::cin >> a;
+    std::cin >> b;
+    int summa = a + b;
+    std::cout << "Сумма: ";
+    std::cout << summa;
+}
